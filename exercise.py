cea2a19fa5f533de63e3203b1e92565a15b9a6b5
GiyongHan <shd02050@naver.com>
2024-12-09 15:28:31 +0900

    FINAL COMMIT

--- exercise.py
+++ exercise.py
@@ -1,7 +1,16 @@
-print("Hello World")
-print(2 + 3)
-print("My name is Giyong Han")
+print("세 정수의 최댓값을 구합니다.")
 
-print("I'm a currently a JOB SEEKER")
+a = int(input("정수 a의 값을 입력하세요.:"))
+b = int(input("정수 b의 값을 입력하세요.:"))
+c = int(input("정수 c의 값을 입력하세요.:"))
 
-print("Well DONE")
+
+maximum = a
+
+if b > maximum:
+    maximum = b
+if c > maximum:
+    maximum = c
+    
+    
+print(f"최댓값은 {maximum}입니다.")
